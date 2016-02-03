6e840deb068d787ce06d59c6f3a444446719b741
hnakai0909 <hnakai0909@yahoo.co.jp>
2016-02-04 03:40:21 +0900

    fix i2c(twe)

--- twi.c
+++ twi.c
@@ -15,6 +15,8 @@ void I2C_Init(void){
 }
 
 void I2C_Start(void){
+	TWCR = _BV(TWINT);
+	_delay_us(27);
 	TWCR = _BV(TWINT) | _BV(TWSTA) | _BV(TWEN);
 	loop_until_bit_is_set(TWCR, TWINT);
 }
@@ -27,6 +29,7 @@ void I2C_Send(uint8_t data){
 	TWDR = data;
 	TWCR = _BV(TWINT) | _BV(TWEN);
 	loop_until_bit_is_set(TWCR, TWINT);
+	//_delay_us(27);
 }
 
 uint8_t I2C_Recv(int ack){
@@ -43,17 +46,13 @@ uint8_t I2C_Recv(int ack){
 
 void I2C_Recv_Num(uint8_t i2caddr, uint8_t* data, uint8_t num){
 	unsigned char i;
-	uint8_t tmp;
 	I2C_Start();
 	I2C_Send((i2caddr << 1) | 1); //Read
-	if(num>1){
-		for(i = 0 ; i < num-1 ; i++){
-			data[i] = I2C_Recv(I2C_ACK);
-		}
-		tmp = I2C_Recv(I2C_NOACK);
-	}else{
-		data[0] = I2C_Recv(I2C_NOACK);
+	for(i = 0 ; i < num ; i++){
+		data[i] = I2C_Recv(I2C_ACK);
+		_delay_us(27);
 	}
+	I2C_Recv(I2C_NOACK);
 	I2C_Stop();
 	TWCR = _BV(TWINT);
 }
